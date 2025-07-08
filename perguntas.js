efe5cb55d570e9df0af2c16cfae4538209c5d4be
Fabi <168186686+Maisagata@users.noreply.github.com>
2025-07-08 10:57:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação que permite a você implementar itens complexos em páginas web'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
